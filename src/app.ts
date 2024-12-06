a2c53fea6226cf64d3942cfd2b2e9b96cda61dcf
GustavoGebhardt <gustavorgebhardt@gmail.com>
2024-12-06 16:05:40 -0300

    Building Dir Structure

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import { corsOptions } from './config/cors'
+import redis from "./config/redis"
+
+const app = express()
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    //redis.set('user:1:name', 'Bob', 'EX', 60)
+    redis.get('user:1:name', (err, valor) => {
+        if (err) {
+            console.error('Erro ao obter o valor:', err)
+        } else {
+            if (valor == null) {
+                return res.send('Valor não encontrado');
+            }
+            res.send(`Valor obtido: ${valor}`)
+        }
+    });
+
+    //res.send('Hello World!')
+})
+
+export default app
---
+++ src/config/redis.ts
@@ -0,0 +1,16 @@
+import Redis from 'ioredis'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+redis.on('connect', () => {
+    console.log('Conectado ao Redis!')
+});
+
+redis.on('error', (err) => {
+    console.error('Erro ao conectar ao Redis:', err)
+});
+
+export default redis
